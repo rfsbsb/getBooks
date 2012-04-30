16a80bfc2a80a145338c6a4d06a74291ac85b17e
Rafael Ferreira Silva <rafael@rafaelsilva.net>
2012-04-30 10:43:44 -0300

    Added more verification about pages and year

--- getbook.php
+++ getbook.php
@@ -72,20 +72,25 @@ class Book {
       $year_index = $number_items -1;
       $pages_index = $number_items;
 
-      $year = $dom->find("//div[@class='espTec']/p[".$year_index."]/text()");
-      if (sizeof($year) > 0) {
-        $year = trim($year[0]);
-        if (is_numeric($year)) {
-          $this->year = $year;
-        }
-      }
-
       $pages = $dom->find("//div[@class='espTec']/p[".$pages_index."]/text()");
       if (sizeof($pages) > 0) {
         $pages = trim($pages[0]);
         if (is_numeric($pages)) {
           $this->pages = $pages;
         }
+      }
+
+      // If there's pages, we can trust year, otherwise we set both null.
+      if ($this->pages) {
+        $year = $dom->find("//div[@class='espTec']/p[".$year_index."]/text()");
+        if (sizeof($year) > 0) {
+          $year = trim($year[0]);
+          if (is_numeric($year)) {
+            $this->year = $year;
+          }
+        }
+      } else {
+        $this->pages = $this->year = NULL;
       }
 
       $cover = $dom->find("//div[@class='boxImg2']/img",'src');
